ff119c1fd500c2dbb6fbdaf15fbe8ec9a72f4a1c
吴令 <wx_da9ca5d160264260a28c6febefd0c909@git.code.tencent.com>
2019-09-20 18:46:46 +0800

    '完善base_ui的封装'

--- tools/api/request_tool.py
+++ tools/api/request_tool.py
@@ -7,86 +7,102 @@ from tools.report import log_tool
 from tools.report.decorators_tool import logs
 import time
 
-@logs
-def get_request(url, params=None, headers=None, cookies=None):
-    """
-    Get请求
-    :param url:
-    :param data:
-    :param header:
-    :return:
-    """
 
+@logs
+def get(url, params=None, headers=None, cookies=None):
     if not (url.startswith('http://') or url.startswith('https://')):
         url = '%s%s' % ('http://', url)
         print(url)
     try:
-
         response = requests.get(url=url, params=params, headers=headers, cookies=cookies)
-
-    except requests.RequestException as e:
+    except Exception as e:
         log_tool.error('%s%s' % ('Exception url: ', url))
         log_tool.error(e)
         return ()
-
-    except Exception as e:
-        log_tool.error('%s%s' % ('Exception url: ', url))
-        return ()
-
     time_consuming = response.elapsed.microseconds / 1000
     log_tool.info('----请求用时: %s 秒数' % time_consuming)
-
     return response
 
 
 @logs
-def post_request(url, data=None,files=None, params=None, headers=None, json=None, cookies=None):
-    """
-    Post请求
-    :param url:
-    :param data:
-    :param header:
-    :return:
-    """
+def post(url, data=None, files=None, params=None, headers=None, json=None, cookies=None):
     if not (url.startswith('http://') or url.startswith('https://')):
         url = '%s%s' % ('http://', url)
         print(url)
     try:
-        response = requests.post(url, data=data,files=files, params=params, headers=headers, json=json, cookies=cookies)
-
-    except requests.RequestException as e:
-        log_tool.error('%s%s' % ('RequestException url: ', url))
-        log_tool.error(e)
-        return ()
-
+        response = requests.post(url, data=data, files=files, params=params, headers=headers, json=json,
+                                 cookies=cookies)
     except Exception as e:
         log_tool.error('%s%s' % ('Exception url: ', url))
         log_tool.error(e)
         return ()
-
     # time_consuming为响应时间，单位为毫秒
     time_consuming = response.elapsed.microseconds / 1000
     log_tool.info('----请求用时: %s 秒数' % time_consuming)
-
     return response
 
 
 @logs
-def post_request_multipart(url, files=None, headers=None, cookies=None):
-    """
-    提交Multipart/form-data 格式的Post请求
-    :param url:
-    :param data:
-    :param header:
-    :param file_parm:
-    :param file:
-    :param type:
-    :return:
-    """
+def post_data(url, data=None, headers=None, cookies=None):
     if not (url.startswith('http://') or url.startswith('https://')):
         url = '%s%s' % ('http://', url)
         print(url)
-    response = requests.post(url=url, files=files, headers=headers, cookies=cookies)
+    try:
+        response = requests.post(url, data=data, headers=headers, cookies=cookies)
+    except Exception as e:
+        log_tool.error('%s%s' % ('Exception url: ', url))
+        log_tool.error(e)
+        return ()
+    # time_consuming为响应时间，单位为毫秒
+    time_consuming = response.elapsed.microseconds / 1000
+    log_tool.info('----请求用时: %s 秒数' % time_consuming)
+    return response
+
+
+@logs
+def post_params(url, params=None, headers=None, cookies=None):
+    if not (url.startswith('http://') or url.startswith('https://')):
+        url = '%s%s' % ('http://', url)
+        print(url)
+    try:
+        response = requests.post(url, params=params, headers=headers, cookies=cookies)
+    except Exception as e:
+        log_tool.error('%s%s' % ('Exception url: ', url))
+        log_tool.error(e)
+        return ()
+    # time_consuming为响应时间，单位为毫秒
+    time_consuming = response.elapsed.microseconds / 1000
+    log_tool.info('----请求用时: %s 秒数' % time_consuming)
+    return response
+
+@logs
+def post_json(url, json=None, headers=None, cookies=None):
+    if not (url.startswith('http://') or url.startswith('https://')):
+        url = '%s%s' % ('http://', url)
+        print(url)
+    try:
+        response = requests.post(url, headers=headers, cookies=cookies)
+    except Exception as e:
+        log_tool.error('%s%s' % ('Exception url: ', url))
+        log_tool.error(e)
+        return ()
+    # time_consuming为响应时间，单位为毫秒
+    time_consuming = response.elapsed.microseconds / 1000
+    log_tool.info('----请求用时: %s 秒数' % time_consuming)
+    return response
+
+
+@logs
+def post_file(url, files=None, headers=None, cookies=None):
+    if not (url.startswith('http://') or url.startswith('https://')):
+        url = '%s%s' % ('http://', url)
+        print(url)
+    try:
+        response = requests.post(url=url, files=files, headers=headers, cookies=cookies)
+    except Exception as e:
+        log_tool.error('%s%s' % ('Exception url: ', url))
+        log_tool.error(e)
+        return ()
     # time_consuming为响应时间，单位为毫秒
     time_consuming = response.elapsed.microseconds / 1000
     log_tool.info('----请求用时: %s 秒数' % time_consuming)
@@ -94,15 +110,9 @@ def post_request_multipart(url, files=None, headers=None, cookies=None):
     return response
 
 
+
 @logs
-def put_request(url, data, header=None, cookies=None):
-    """
-    Put请求
-    :param url:
-    :param data:
-    :param header:
-    :return:
-    """
+def put(url, data, header=None, cookies=None):
     if not (url.startswith('http://') or url.startswith('https://')):
         url = '%s%s' % ('http://', url)
         log_tool.debug(url)
@@ -113,20 +123,15 @@ def put_request(url, data, header=None, cookies=None):
         else:
             response = requests.put(url=url, params=data, headers=header, cookies=cookies)
 
-    except requests.RequestException as e:
+    except Exception as e:
         log_tool.error('%s%s' % ('RequestException url: ', url))
         log_tool.error(e)
         return ()
-
-    except Exception as e:
-        print('%s%s' % ('Exception url: ', url))
-        print(e)
-        return ()
-
     time_consuming = response.elapsed.microseconds / 1000
     log_tool.info('----请求用时: %s 秒数' % time_consuming)
 
     return response
+
 
 def down_big_file(srcUrl, localFile):
     print('%s\n --->>>\n  %s' % (srcUrl, localFile))
@@ -145,7 +150,7 @@ def down_big_file(srcUrl, localFile):
                 downSize += len(chunk)
                 line = '%d KB/s - %.2f MB， 共 %.2f MB'
                 line = line % (
-                downSize / 1024 / (time.time() - startTime), downSize / 1024 / 1024, contentLength / 1024 / 1024)
+                    downSize / 1024 / (time.time() - startTime), downSize / 1024 / 1024, contentLength / 1024 / 1024)
                 print(line, end='\r')
                 if downSize >= contentLength:
                     break
@@ -154,8 +159,9 @@ def down_big_file(srcUrl, localFile):
         line = line % (timeCost, downSize / 1024 / timeCost)
         print(line)
 
-def copy_github_file(url,save_name):
-    resp = get_request(url)
+
+def copy_github_file(url, save_name):
+    resp = get(url)
     body = resp.text
     with open(save_name, 'w', encoding='utf-8') as file:
         file.write(body)
